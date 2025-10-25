1dd78f8266f2e3b3ffabfcc60496f7d2b640cc67
Julien Baouz <baouzjulien@gmail.com>
2025-10-25 12:24:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const pyramid = (string, integer, boolean) => {
-  let i = -1;
-  let j = integer;
-  let space = " ";
-  while(i < integer) {
-    i+=2;
-    j-=1;
-    let pyramidConstruc = `${space.repeat(j)} ${string.repeat(i)}`;
-    console.log(pyramidConstruc);
-  };
+const pyramid = (char, height, inverted) => {
+  let result = "\n";
+
+  for (let i = 0; i < height; i++) {
+    const level = inverted ? height - i - 1 : i;
+    const spaces = " ".repeat(height - level - 1);
+    const blocks = char.repeat(level * 2 + 1);
+
+    result += spaces + blocks + "\n";
+  }
+
+  return result;
 };
 
-pyramid("O", 10)
-
-
-/* ---0
-   --000
-   -00000
-   0000000 */
+// Tests
+console.log(pyramid("0", 10, false));
+console.log(pyramid("0", 10, true));
